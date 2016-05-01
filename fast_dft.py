c02268c42f5cba75a332b8f9e25786557e09d979
Ian Sullivan <sullii@uw.edu>
2016-04-30 20:10:51 -0700

    Move internal functions out of the main body and add comments

--- fast_dft.py
+++ fast_dft.py
@@ -1,3 +1,25 @@
+#
+# LSST Data Management System
+# Copyright 2016 LSST Corporation.
+#
+# This product includes software developed by the
+# LSST Project (http://www.lsst.org/).
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the LSST License Statement and
+# the GNU General Public License along with this program.  If not,
+# see <http://www.lsstcorp.org/LegalNotices/>.
+#
+
 """
 A fast DFT approximation for arrays of amplitudes at floating-point locations.
 
@@ -11,12 +33,9 @@ for an MxN image.
 
 If amplitudes is a two dimensional array (m, n), it is interpreted as a 1D array of m points,
 each with n different amplitude values. In this case, a n element list of MxN images will be returned.
-
-Important note:
 """
 from __future__ import division
 import numpy as np
-import cPickle
 import unittest
 import lsst.utils.tests as utilsTests
 
@@ -24,13 +43,18 @@ import lsst.utils.tests as utilsTests
 def fast_dft(amplitudes, x_loc, y_loc, x_size=None, y_size=None, no_fft=True, kernel_radius=10, **kwargs):
     """
     !Construct a gridded 2D Fourier transform of an array of amplitudes at floating-point locations.
-    x_loc - 1D floating point array of x pixel coordinates.
-    y_loc - 1D floating point array of y pixel coordinates.
-    x_size - desired number of pixels N for the output (M, N) image.
-    y_size - desired number of pixels M for the output (M, N) image.
-    no_fft - if True, returns the sinc-interpolated image, otherwise takes the FFT [default: True]
-    kernel_radius - number of pixels to either side of each source to include [default: 10]
+    @param amplitudes: Either a 1D floating point array of amplitudes for each x_loc and y_loc,
+                       or a 2D floating point array with a vector of values for each x_loc and y_loc
+    @param x_loc: 1D floating point array of x pixel coordinates.
+    @param y_loc: 1D floating point array of y pixel coordinates.
+    @param x_size: desired number of pixels N for the output (M, N) image.
+    @param y_size: desired number of pixels M for the output (M, N) image.
+    @param no_fft: if True, returns the sinc-interpolated image, otherwise takes the FFT [default: True]
+    @param kernel_radius: number of pixels to either side of each source to include [default: 10]
     Accepts **kwargs so that these parameters can be passed through wrappers.
+
+    @return: if amplitudes is 1D, a numpy ndarray of size (y_size, x_size)
+             if amplitudes is 2D, a list of numpy ndarrays as above
     """
     pi = np.pi
 
@@ -53,81 +77,12 @@ def fast_dft(amplitudes, x_loc, y_loc, x_size=None, y_size=None, no_fft=True, ke
     else:
         full_flag = False
 
-    def kernel_1d(locs, size):
-        """pre-compute the 1D sinc function values along each axis."""
-        pix = np.arange(size, dtype=np.float64)
-        sign = np.power(-1.0, pix)
-        offset = np.floor(locs)
-        delta = locs - offset
-        kernel = np.zeros((len(locs), size), dtype=np.float64)
-        for i, loc in enumerate(locs):
-            if delta[i] == 0:
-                kernel[i, :][offset[i]] = 1.0
-            else:
-                kernel[i, :] = np.sin(-pi * loc) / (pi * (pix - loc)) * sign
-        return kernel
-
-    def kernel_1d_gen(locs, size):
-        """A generalized generator function that pre-computes the 1D sinc function values along one axis."""
-        pix = np.arange(size, dtype=np.float64)
-        sign = np.power(-1.0, pix)
-        for loc in locs:
-            offset = np.floor(loc)
-            delta = loc - offset
-            if delta == 0:
-                kernel = np.zeros(size, dtype=np.float64)
-                kernel[offset] = 1.0
-            else:
-                kernel = np.sin(-pi * loc) / (pi * (pix - loc))
-                kernel *= sign
-            yield kernel
     kernel_x_gen = kernel_1d_gen(x_loc, x_size)
     kernel_y_gen = kernel_1d_gen(y_loc, y_size)
 
     if multi_catalog:
-
-        def kernel_circle_inds(x_loc, y_loc, kernel_radius=None):
-            """
-            A generator function that pre-computes the pixels to use for gridding.
-            Returns the x and y indices for all pixels within a given radius of a location,
-            that are NOT included in slices centered on that location.
-            Also applies a Hanning window function for those values, to reduce ringing at the edges.
-            """
-            ind_radius = int(4 * kernel_radius)
-            x_i0, y_i0 = np.meshgrid(np.arange(2.0 * ind_radius), np.arange(2.0 * ind_radius))
-            x_i0_int, y_i0_int = np.meshgrid(np.arange(2 * ind_radius), np.arange(2 * ind_radius))
-            x_pix_arr = np.round(x_loc)
-            y_pix_arr = np.round(y_loc)
-            taper_filter = np.hanning(2 * ind_radius)
-            taper_filter /= taper_filter[ind_radius - kernel_radius]
-            for src_i in range(len(x_loc)):
-                x_pix = int(x_pix_arr[src_i])
-                y_pix = int(y_pix_arr[src_i])
-                dx = x_loc[src_i] - x_pix + ind_radius
-                dy = y_loc[src_i] - y_pix + ind_radius
-
-                dist = np.sqrt((x_i0 - dx)**2.0 + (y_i0 - dy)**2.0)
-                dist[ind_radius - kernel_radius: ind_radius + kernel_radius, :] = ind_radius
-                dist[:, ind_radius - kernel_radius: ind_radius + kernel_radius] = ind_radius
-                if x_pix < ind_radius:
-                    dist[:, 0: ind_radius - x_pix] = ind_radius
-                if x_pix > x_size - ind_radius:
-                    dist[:, x_size - ind_radius - x_pix:] = ind_radius
-                if y_pix < ind_radius:
-                    dist[0: ind_radius - y_pix, :] = ind_radius
-                if y_pix > y_size - ind_radius:
-                    dist[y_size - ind_radius - y_pix:, :] = ind_radius
-                dist_test = dist < ind_radius
-                x_i = x_i0_int[dist_test]
-                y_i = y_i0_int[dist_test]
-                # y_i, x_i = np.where(test_image < ind_radius)
-                taper = taper_filter[y_i] * taper_filter[x_i]
-                x_i += x_pix - ind_radius
-                y_i += y_pix - ind_radius
-                yield x_i
-                yield y_i
-                yield taper
-
+        # If each location (source) has a vector of amplitudes, calculate the coefficients for each location,
+        # and simply multiply those coefficients by each amplitude value to build an array of model images
         if full_flag:
             model_img = []
             amp_arr = [amplitudes[:, _i] for _i in range(n_cat)]
@@ -140,12 +95,12 @@ def fast_dft(amplitudes, x_loc, y_loc, x_size=None, y_size=None, no_fft=True, ke
             amp_arr = [amplitudes[_i, :] for _i in range(len(x_loc))]
             model_arr = np.zeros((y_size, x_size, n_cat))
             model_arr_T = np.zeros((x_size, y_size, n_cat))
-            x_pix = (int(np.round(xv)) for xv in x_loc)
-            y_pix = (int(np.round(yv)) for yv in y_loc)
-            kernel_ind_gen = kernel_circle_inds(x_loc, y_loc, kernel_radius=kernel_radius)
-            for amp in amp_arr:
-                x_c = next(x_pix)
-                y_c = next(y_pix)
+            kernel_ind_gen = kernel_circle_inds(x_loc, y_loc, x_size=x_size, y_size=y_size,
+                                                kernel_radius=kernel_radius)
+            for _i, amp in enumerate(amp_arr):
+                # Calculate the sinc-interpolated model image for each source
+                x_c = int(np.round(x_loc[_i]))
+                y_c = int(np.round(y_loc[_i]))
                 x0 = x_c - kernel_radius
                 if x0 < 0:
                     x0 = 0
@@ -164,7 +119,6 @@ def fast_dft(amplitudes, x_loc, y_loc, x_size=None, y_size=None, no_fft=True, ke
                 # central pixels will be added in both slices, so set to zero in one of them
                 kernel_x_slice[:, y0:y1] = 0
                 kernel_y_slice = np.einsum('i,j->ij', kernel_y[y0:y1], kernel_x)
-                # kernel_y_slice[:, x0:x1] /= 2.0
                 x_i = next(kernel_ind_gen)
                 y_i = next(kernel_ind_gen)
                 taper = next(kernel_ind_gen)
@@ -193,6 +147,103 @@ def fast_dft(amplitudes, x_loc, y_loc, x_size=None, y_size=None, no_fft=True, ke
         else:
             model_img = np.fft.rfft2(model_img)
     return(model_img)
+
+
+def kernel_1d(locs, size):
+    """
+    pre-compute the 1D sinc function values along each axis.
+
+    @param locs: pixel coordinates of dft locations along single axis (either x or y)
+    @params size: dimension in pixels of the given axis
+    """
+    pi = np.pi
+    pix = np.arange(size, dtype=np.float64)
+    sign = np.power(-1.0, pix)
+    offset = np.floor(locs)
+    delta = locs - offset
+    kernel = np.zeros((len(locs), size), dtype=np.float64)
+    for i, loc in enumerate(locs):
+        if delta[i] == 0:
+            kernel[i, :][offset[i]] = 1.0
+        else:
+            kernel[i, :] = np.sin(-pi * loc) / (pi * (pix - loc)) * sign
+    return kernel
+
+
+def kernel_1d_gen(locs, size):
+    """
+    A generalized generator function that pre-computes the 1D sinc function values along one axis.
+
+    @param locs: pixel coordinates of dft locations along single axis (either x or y)
+    @params size: dimension in pixels of the given axis
+    @return: yields the sinc interpolation along a single axis for each loc
+    """
+    pi = np.pi
+    pix = np.arange(size, dtype=np.float64)
+    sign = np.power(-1.0, pix)
+    for loc in locs:
+        offset = np.floor(loc)
+        delta = loc - offset
+        if delta == 0:
+            kernel = np.zeros(size, dtype=np.float64)
+            kernel[offset] = 1.0
+        else:
+            kernel = np.sin(-pi * loc) / (pi * (pix - loc))
+            kernel *= sign
+        yield kernel
+
+
+def kernel_circle_inds(x_loc, y_loc, x_size=None, y_size=None, kernel_radius=None):
+        """
+        A generator function that pre-computes the pixels to use for gridding.
+        Returns the x and y indices for all pixels within a given radius of a location,
+        that are NOT included in slices centered on that location.
+        Also applies a Hanning window function for those values, to reduce ringing at the edges.
+
+        @param x_loc: 1D floating point array of x pixel coordinates of the sources
+        @param y_loc: 1D floating point array of y pixel coordinates of the sources
+        @param x_size: dimension of the full x-axis
+        @param y_size: dimension of the full y-axis
+        @param kernel_radius: radius, in floating point pixels, to include around each (y_loc, x_loc)
+        @return: yields three quantities in succession, each an array:
+                    x_i: x index of each pixel within the given radius
+                    y_i: y index of each pixel within the given radius
+                    taper: value of the antialiasing filter at each pixel within the given radius
+        """
+        ind_radius = int(4 * kernel_radius)
+        x_i0, y_i0 = np.meshgrid(np.arange(2.0 * ind_radius), np.arange(2.0 * ind_radius))
+        x_i0_int, y_i0_int = np.meshgrid(np.arange(2 * ind_radius), np.arange(2 * ind_radius))
+        x_pix_arr = np.round(x_loc)
+        y_pix_arr = np.round(y_loc)
+        taper_filter = np.hanning(2 * ind_radius)
+        taper_filter /= taper_filter[ind_radius - kernel_radius]
+        for src_i in range(len(x_loc)):
+            x_pix = int(x_pix_arr[src_i])
+            y_pix = int(y_pix_arr[src_i])
+            dx = x_loc[src_i] - x_pix + ind_radius
+            dy = y_loc[src_i] - y_pix + ind_radius
+
+            dist = np.sqrt((x_i0 - dx)**2.0 + (y_i0 - dy)**2.0)
+            dist[ind_radius - kernel_radius: ind_radius + kernel_radius, :] = ind_radius
+            dist[:, ind_radius - kernel_radius: ind_radius + kernel_radius] = ind_radius
+            if x_pix < ind_radius:
+                dist[:, 0: ind_radius - x_pix] = ind_radius
+            if x_pix > x_size - ind_radius:
+                dist[:, x_size - ind_radius - x_pix:] = ind_radius
+            if y_pix < ind_radius:
+                dist[0: ind_radius - y_pix, :] = ind_radius
+            if y_pix > y_size - ind_radius:
+                dist[y_size - ind_radius - y_pix:, :] = ind_radius
+            dist_test = dist < ind_radius
+            x_i = x_i0_int[dist_test]
+            y_i = y_i0_int[dist_test]
+            # y_i, x_i = np.where(test_image < ind_radius)
+            taper = taper_filter[y_i] * taper_filter[x_i]
+            x_i += x_pix - ind_radius
+            y_i += y_pix - ind_radius
+            yield x_i
+            yield y_i
+            yield taper
 
 
 def input_type_check(var):
